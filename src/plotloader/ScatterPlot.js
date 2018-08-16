79843fc444feddbeba61bb23cfb0f13efa371290
lingyun1010 <zhaolingyun1010@gmail.com>
2018-08-16 16:00:58 +0100

    Add +/- button, yaxis panning, shrink point size

--- src/plotloader/ScatterPlot.js
+++ src/plotloader/ScatterPlot.js
@@ -3,31 +3,65 @@ import PropTypes from 'prop-types'
 import ReactHighcharts from 'react-highcharts'
 import HighchartsExporting from 'highcharts/modules/exporting'
 import HighchartsBoost from 'highcharts/modules/boost'
-
+import HighchartsHeatmap from 'highcharts/modules/heatmap.js'
+import HighchartsMap from 'highcharts-map'
 import deepmerge from 'deepmerge'
 
+import HeatsmapLegend from './HeatsmapLegend'
 import SeriesPropTypes from './SeriesPropTypes'
+import Yaxispanning from './Yaxispanning'
+
 
 const Highcharts = ReactHighcharts.Highcharts
 // Only apply modules if Highcharts isn’t a *good* mock -- Boost/Exporting can break tests
 // if (Highcharts.getOptions()) {
   HighchartsExporting(Highcharts)
   HighchartsBoost(Highcharts)
-// }
+  HighchartsMap(Highcharts)
+  HighchartsHeatmap(Highcharts)
+  HeatsmapLegend(Highcharts)
+  Yaxispanning(Highcharts)
+
 
 const highchartsBaseConfig = {
   credits: {
     enabled: false
   },
-  legend: {
-    labelFormat: `{name}`
-  },
+   
   chart: {
     type: `scatter`,
-    zoomType: `xy`,
+    //zoomType: `xy`,
     borderWidth: 1,
     borderColor: `dark blue`,
-    height: `100%`
+    height: `100%`,
+    panning: true,
+    spacingTop: 50,
+    //panKey: 'shift'
+  },
+  //subtitle: {
+  //  text: 'Click and drag to zoom in. Hold down shift key to pan.'
+  //},
+  mapNavigation: {
+    enabled: true,
+    enableMouseWheelZoom: false,
+    buttonOptions: {
+      theme: {
+        fill: 'white',
+        'stroke-width': 1,
+        stroke: 'silver',
+        r: 0,
+        states: {
+            hover: {
+                fill: '#a4edba'
+            },
+            select: {
+                stroke: '#039',
+                fill: '#a4edba'
+            }
+      }
+    },
+    verticalAlign: 'bottom',
+    }
   },
   boost: {
     useGPUTranslations: true,
@@ -35,7 +69,11 @@ const highchartsBaseConfig = {
     seriesThreshold: 5000
   },
   title: {
-    text: null
+    text: null,
+    style: {
+      fontSize: `25px`,
+      fontWeight: 'bold'
+    }
   },
   xAxis: {
     title: {
@@ -54,7 +92,9 @@ const highchartsBaseConfig = {
       enabled: false
     },
     gridLineWidth: 0,
-    lineWidth: 1
+    lineWidth: 1,
+    endOnTick: false,
+    startOnTick: false
   },
   colors: [`#b25fbc`, `#76b341`, `#6882cf`, `#ce9b44`, `#c8577b`, `#4fae84`, `#c95c3f`, `#7c7f39`],
   plotOptions: {
@@ -76,7 +116,7 @@ const ScatterPlot = (props) => {
         plotOptions: {
           series: {
             marker: {
-              radius: numPoints < 5000 ? 4 : 0.2
+              radius: numPoints < 1000 ? 3 : 3
             }
           }
         }
@@ -88,9 +128,8 @@ const ScatterPlot = (props) => {
   return [
     <div key={`chart`} className={chartClassName}>
       <ReactHighcharts config={config}/>
-    </div>,
+    </div>
 
-    <div key={`children`}>{children}</div>
   ]
 }
 
